5005ebad49dabaf3e1369d268cda50a63fc6744f
Andre Coelho <andrefialhoc@gmail.com>
2021-11-06 12:43:25 +0100

    added so3 exponential functions

--- roblib.py
+++ roblib.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def rotation_matrix(angle,axis):	
-	k_hat=cross_matrix(axis)
-	return np.identity(3)+np.sin(angle)*k_hat+(1-np.cos(angle))*k_hat.dot(k_hat)
+	h_hat=skew(axis)
+	return np.identity(3)+np.sin(angle)*h_hat+(1-np.cos(angle))*h_hat.dot(h_hat)
 
 def rot_x(angle):	
 	return np.array(((1, 0, 0),(0, np.cos(angle), -np.sin(angle)), (0, np.sin(angle), np.cos(angle))))
@@ -13,7 +13,7 @@ def rot_y(angle):
 def rot_z(angle):	
 	return np.array((( np.cos(angle), -np.sin(angle),0), (np.sin(angle), np.cos(angle),0),(0, 0, 1)))
 				
-def cross_matrix(v):	
+def skew(v):	
 	return np.array(((0, -v[2], v[1]),(v[2], 0, -v[0]), (-v[1], v[0], 0)))
 
 def integrator(u,y0,dT):
@@ -36,3 +36,11 @@ def rectangularShape(center,width,height,angle):
   rightTop=np.array([[np.cos(angle),-np.sin(angle)],[np.sin(angle),np.cos(angle)]]).dot(np.array([width/2,height/2]))+center
   points = [leftBottom,rightBottom,rightTop,leftTop]
   return points
+
+  def exp_so3(phi):
+  normPhi=np.linalg.norm(phi)
+  if normPhi<1e-15:
+    return np.identity(3)
+  else:
+    phi_hat=skew(phi)
+    return np.identity(3)+np.sin(normPhi)*phi_hat/normPhi+(1-np.cos(normPhi))*phi_hat.dot(phi_hat)/(normPhi*normPhi)
